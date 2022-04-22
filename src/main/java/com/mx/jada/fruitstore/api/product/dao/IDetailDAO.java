b6532cf064707ccf0eecc6a0ad4ced7f79ddd512
Daniel Velazquez <daniel.velazquez.sallago.contractor@bbva.com>
2022-04-21 23:15:00 -0500

    Revert "Se agregar el default de config de spring"
    This reverts commit d1d88e2ad2a2cfddb7b3767f866aa74877f0ea07.

--- src/main/java/com/mx/jada/fruitstore/api/product/dao/IDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.mx.jada.fruitstore.product.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.mx.jada.fruitstore.products.entity.Detail;
-
-public interface IDetailDAO extends CrudRepository<Detail, Long>{
-
-}
--- src/main/java/com/mx/jada/fruitstore/api/products/service/DetailService.java
+++
@@ -1,9 +0,0 @@
-package com.mx.jada.fruitstore.service;
-
-import com.mx.jada.fruitstore.products.entity.Detail;
-
-public interface DetailService {
-
-	
-	public Detail saveDetail(Detail detail);
-}
